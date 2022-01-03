6cd290337d538a209bb173277a0289078537f93e
JonCabot <jfc0053@gmail.com>
2022-01-03 17:30:36 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 // Your code here
 class Polygon{
-    constructor(...sides) {
-      this.sides = sides;
-    }
-    get countSides(){
-      for(let side of this.sides) {
-        return side.length
-      }
-    }
+  constructor(sides) {
+    this.sides = sides;
+  }
+  get countSides(){
+    return this.sides.length
+  }
 
-    get perimeter(){  
-        return this.sides.reduce((sum, side) => sum + side, 0);
+  get perimeter(){  
+    return this.sides.reduce((sum, side) => sum + side, 0);
+  }
+}  
+
+class Triangle extends Polygon {
+  get isValid(){
+    this.a = this.sides[0];
+    this.b = this.sides[1];
+    this.c = this.sides[2];
+      if(!this.a || !this.b || !this.c){
+          return false;
+      };
+    const con1 = this.a + this.b > this.c;
+    const con2 = this.b + this.c > this.a;
+    const con3 = this.c + this.a > this.b;
+      return con1 && con2 && con3;  
+  };
+} 
+    
+
+class Square extends Polygon {
+  get isValid(){
+    this.a = this.sides[0];
+    this.b = this.sides[1];
+    this.c = this.sides[2];
+    this.d = this.sides[3];
+      if(this.a === this.b && this.a === this.c && this.a === this.d){
+        return true; 
+      } else {
+        return false;
       }
   }
 
-class Triangle extends Polygon {
-    constructor(a, b, c){
-    super(...sides);
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    }
-    get isValid(){
-        if(!this.a || !this.b || !this.c){
-            return false;
-         };
-         const con1 = this.a + this.b > this.c;
-         const con2 = this.b + this.c > this.a;
-         const con3 = this.c + this.a > this.b;
-         return con1 && con2 && con3;
-      };
-    }
-
-
-class Square extends Polygon {
+  get area(){
+    this.a = this.sides[0];
+    this.b = this.sides[1];
+      if(this.a !== this.b){
+        return false;
+      } else {
+        return this.a * this.b;
+      }
+  }
 
 }
